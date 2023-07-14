0764f66f78ccee71ffa48a423c5c30c03fa0464c
rajnishxoxo <64491136+rajnishxoxo@users.noreply.github.com>
2023-07-14 12:25:56 +0530

    writing checkGameOver function

--- index.js
+++ index.js
@@ -1,92 +1,96 @@
-let currentPlayerStatus = document.querySelector('.player-info-current');
-let boxes = document.querySelectorAll('.box');
+let currentPlayerStatus = document.querySelector(".player-info-current");
+let boxes = document.querySelectorAll(".box");
 
-const newGameButton = document.getElementById('btn');
+const newGameButton = document.getElementById("btn");
 
 let gameGrid;
 
 let currentPlayer;
 
-const winningPosition =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+const winningPosition = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
 //Step-1 : To initlize the Game.
 
- const startGame=(function initlizeGame(){
-    currentPlayer ="X"
-    currentPlayerStatus.innerHTML=`Current Player : ${currentPlayer}`
-    boxes.forEach((box,index)=>{
-        box.innerText=""
-        boxes[index].style.pointerEvents = "all";
-       })
-    gameGrid =["","","","","","","","",""]
-
+const startGame = (function initlizeGame() {
+  currentPlayer = "X";
+  currentPlayerStatus.innerHTML = `Current Player : ${currentPlayer}`;
+  boxes.forEach((box, index) => {
+    box.innerText = "";
+    boxes[index].style.pointerEvents = "all";
+  });
+  gameGrid = ["", "", "", "", "", "", "", "", ""];
 })();
-
-
 
 //Step-2 : To add event listner to all the boxes.
 
-boxes.forEach((box,index)=>{
-    box.addEventListener('click',()=>{
-        handleClick(index);
-    })
-})
+boxes.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    handleClick(index);
+  });
+});
 
+// define what will handleClick function will do.
 
-// define what will handleClick function will do. 
+function handleClick(index) {
+  if (gameGrid[index] === "") {
+    //this is for Updating UI
+    boxes[index].innerHTML = currentPlayer;
+    //this is for Updating our Game Logic.
 
-function handleClick(index){
+    gameGrid[index] = currentPlayer;
 
-    if(gameGrid[index]===""){
-        //this is for Updating UI
-       boxes[index].innerHTML=currentPlayer;
-       //this is for Updating our Game Logic.
+    console.log(gameGrid);
 
-       gameGrid[index]=currentPlayer;
+    boxes[index].style.pointerEvents = "none";
 
-       boxes[index].style.pointerEvents = "none";
-       
-       swapPlayer();
+    swapPlayer();
 
-       checkGameOver();
-
-    }
+    //    checkGameOver();
+  }
 }
 
 //Step-3 : SwapPlayers
 
-function swapPlayer(){
-    if(currentPlayer=="X"){
-        currentPlayer="O"
-        currentPlayerStatus.innerHTML = `Current Player : ${currentPlayer}`
-    }else{
-        currentPlayer="X"
-        currentPlayerStatus.innerHTML = `Current Player : ${currentPlayer}`
-    }
+function swapPlayer() {
+  if (currentPlayer == "X") {
+    currentPlayer = "O";
+    currentPlayerStatus.innerHTML = `Current Player : ${currentPlayer}`;
+  } else {
+    currentPlayer = "X";
+    currentPlayerStatus.innerHTML = `Current Player : ${currentPlayer}`;
+  }
 }
 
+newGameButton.addEventListener("click", () => {
+  currentPlayer = "X";
+  currentPlayerStatus.innerHTML = `Current Player : ${currentPlayer}`;
+  boxes.forEach((box, index) => {
+    box.innerText = "";
+    boxes[index].style.pointerEvents = "all";
+  });
+  gameGrid = ["", "", "", "", "", "", "", "", ""];
+});
 
-newGameButton.addEventListener('click' , ()=>{
-    currentPlayer ="X"
-    currentPlayerStatus.innerHTML=`Current Player : ${currentPlayer}`
-    boxes.forEach((box,index)=>{
-        box.innerText=""
-        boxes[index].style.pointerEvents = "all";
-       })
-    gameGrid =["","","","","","","","",""]
-})
+function checkGameOver() {
 
+    let hasWon = false;
 
-function checkGameOver(){
-    
+  for (let i = 0; i < winningPosition.length; i++) {
+   let condition = winningPosition[i];
+    const cellA = gameGrid[condition[0]];
+    const cellB = gameGrid[condition[1]];
+    const cellC = gameGrid[condition[2]];
+
+  }
 }
 
+checkGameOver();
